686de2ca9e95fff98b8a3bae939c12d50c601531
Ovsjah Schweinefresser <ovsjah@gmail.com>
2017-06-03 14:29:28 +0300

    Update caesar_cipher.rb

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -6,12 +6,11 @@ def caesar_cipher(str, shift)
     if (65..90) === int && 90 - int < shift
       n_char = (65 + (shift - (90 - int) - 1)).chr
       n_str << n_char
-      p n_str
-    elsif 122 - char.ord < shift
-      n_char = (97 + (shift - (122 - char.ord) - 1)).chr
+    elsif 122 - int < shift
+      n_char = (97 + (shift - (122 - int) - 1)).chr
       n_str << n_char
     elsif (97..122) === int || (65..90) === int
-      n_char = (char.ord + shift).chr
+      n_char = (int + shift).chr
       n_str << n_char
     else
       n_str << char
